8a8a8b23b8343ef69c3b60cbcc5b54a34160f0a9
vaibhavyb <knvaibhav55@gmail.com>
2021-11-15 12:56:11 +0530

    'updated'

--- src/components/OrderSummary/Orders.js
+++ src/components/OrderSummary/Orders.js
@@ -4,22 +4,21 @@ import OrderItem from "./OrderItem";
 function Orders(props) {
   return (
     <div>
-      {props.data.length!=0 &&(<h3 style={{textAlign:"center"}}>ORDER SUMMARY</h3>)}
-      
-      {props.data.map(item => (
-        item.total>0 && 
+      {props.data.length != 0 && (
+        <h3 style={{ textAlign: "center" }}>ORDER SUMMARY</h3>
+      )}
 
-        <OrderItem
-          
-          food={item.food}
-          quantity={item.total}
-        />
-      ))}
+      {props.data.map(
+        item =>
+          item.total > 0 && (
+            <OrderItem key={item.id} food={item.food} quantity={item.total} />
+          )
+      )}
 
       <div>
         {props.data.length != 0 && (
           <span>
-            <p style={{textAlign:"center"}}>Have a gr8 day,Eat well...</p>
+            <p style={{ textAlign: "center" }}>Have a gr8 day,Eat well...</p>
           </span>
         )}
       </div>
